7ee1519eb4da2f1a8897fc09a528b2d6e052a30a
Alejandro261102 <alejandrohdz.2626@gmail.com>
2026-05-30 21:37:22 -0600

    application.properties

--- Frontend-React/src/services/api.js
+++ Frontend-React/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Utiliza la variable de entorno para producción o localhost para desarrollo
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
--- Frontend-React/src/services/searchService.js
+++ Frontend-React/src/services/searchService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api/search';
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 const authHeader = () => {
   const user = JSON.parse(localStorage.getItem('user')); 
